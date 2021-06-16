4fd9085f8dea2375fa8226f387c6066a424266ac
limbo <yykaue@qq.com>
2021-06-16 10:37:47 +0800

    limbo-common-css done

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# limbo-common-css
-UI样式仓库
+## limbo-common-css
+
+   通用样式库
+
+## 安装方法
+
+    npm i limbo-common-css -s
+
+
+## 使用说明
---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ *Created by limbo <yykaue@qq.com> on 2019/7/3.
+ */
+import index from './lib/index/index.scss'
+import fontCrm from './lib/font-crm/iconfont.css'
+
+export {
+  index,
+  fontCrm
+}
+export default {
+  index,
+  fontCrm
+}
