3b7475bcf62335a7a545af57010735861649d57e
limbo <yykaue@qq.com>
2021-07-26 17:40:48 +0800

    new project

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  *Created by limbo <yykaue@qq.com> on 2019/7/3.
  */
 import index from './lib/index/index.scss'
-import fontCrm from './lib/font-crm/iconfont.css'
+import fontCrm from './lib/iconfont/iconfont.css'
 
 export {
   index,
